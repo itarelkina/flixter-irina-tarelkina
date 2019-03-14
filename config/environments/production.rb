ef647ab6821efbedc0ad86dd9886c0423bbb4a6f
Irina Tarelkina <itarelkina@yahoo.ca>
2019-03-14 15:11:48 +0000

    Fixing Heroku rename

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,10 @@
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { host: 'flixter-irina-tarelkina.herokuapp.com' )
+  config.action_mailer.default_url_options = { host: 'flixter-irina-tarelkina.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
