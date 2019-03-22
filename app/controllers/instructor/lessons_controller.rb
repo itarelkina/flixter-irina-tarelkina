36afa277d29a9a37d8beb83b693acf7f8387c268
Irina Tarelkina <itarelkina@yahoo.ca>
2019-03-22 17:41:37 +0000

    Permissions for Lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,16 +1,27 @@
 class Instructor::LessonsController < ApplicationController
+    before_action :authenticate_user!
+  
     def new
-      @section = Section.find(params[:section_id])
+      if current_section.course.user != current_user
+        return render plain: 'Unauthorized', status: :unauthorized
+      end
       @lesson = Lesson.new
     end
   
     def create
-      @section = Section.find(params[:section_id])
-      @lesson = @section.lessons.create(lesson_params)
-      redirect_to instructor_course_path(@section.course)
+      if current_section.course.user != current_user
+        return render plain: 'Unauthorized', status: :unauthorized
+      end
+      @lesson = current_section.lessons.create(lesson_params)
+      redirect_to instructor_course_path(current_section.course)
     end
   
     private
+  
+    helper_method :current_section
+    def current_section
+      @current_section ||= Section.find(params[:section_id])
+    end
   
     def lesson_params
       params.require(:lesson).permit(:title, :subtitle)
