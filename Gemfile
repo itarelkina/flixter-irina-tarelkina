9be4b58a5a9695cb037e73e7ab2f591f4bae1319
Irina Tarelkina <itarelkina@yahoo.ca>
2019-03-27 01:29:33 +0000

    Ordering Lessons in Our Database

--- Gemfile
+++ Gemfile
@@ -77,3 +77,4 @@ gem "fog-aws"
 gem "mini_magick"
 gem 'carrierwave-aws'
 gem 'stripe'
+gem 'ranked-model'
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,7 @@ class Lesson < ApplicationRecord
 
   validates :title, presence: true
   validates :subtitle, presence: true
+
+  include RankedModel
+  ranks :row_order, with_same: :section_id
 end
