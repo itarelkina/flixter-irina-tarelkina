734f747205efba2e170d69b330ce3c72f3f897fe
Irina Tarelkina <itarelkina@yahoo.ca>
2019-04-01 18:43:39 +0000

    fixed section button to work

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,6 @@
 class Model < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :trackable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
